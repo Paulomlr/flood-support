211044947e9257166ea6885584f9e023dfa97148
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-10-21 13:59:24 -0300

    Project created

---
+++ src/main/java/com/paulo/FloodSupport/FloodSupportApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.FloodSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/FloodSupport/FloodSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.FloodSupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
