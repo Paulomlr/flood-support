9595afb8a0187e64e4c6c2b6fc02cb6be1bbdf19
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-10-24 19:21:36 -0300

    feat: add AllSheltersUseCase

---
+++ src/main/java/com/paulo/FloodSupport/core/usecase/shelter/AllSheltersUseCase.java
@@ -0,0 +1,10 @@
+package com.paulo.FloodSupport.core.usecase.shelter;
+
+import com.paulo.FloodSupport.core.domain.Shelter;
+
+import java.util.List;
+
+public interface AllSheltersUseCase {
+
+    List<Shelter> getAllShelters();
+}
