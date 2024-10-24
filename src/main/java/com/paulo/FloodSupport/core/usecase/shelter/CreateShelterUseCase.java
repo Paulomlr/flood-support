a939f62f50b826d08bbf07e65e7db12de040de6d
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-10-24 19:22:45 -0300

    feat: CreateShelterUseCase

---
+++ src/main/java/com/paulo/FloodSupport/core/usecase/shelter/CreateShelterUseCase.java
@@ -0,0 +1,8 @@
+package com.paulo.FloodSupport.core.usecase.shelter;
+
+import com.paulo.FloodSupport.core.domain.Shelter;
+
+public interface CreateShelterUseCase {
+
+    void create(Shelter shelter);
+}
