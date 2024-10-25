8f8cd645e5a802a1b4b523b43ae921afc19d3fb8
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-10-25 20:39:03 -0300

    feat: add AllDistributionCenterUseCase

---
+++ src/main/java/com/paulo/FloodSupport/core/usecase/distributionCenter/AllDistributionCenterUseCase.java
@@ -0,0 +1,10 @@
+package com.paulo.FloodSupport.core.usecase.distributionCenter;
+
+import com.paulo.FloodSupport.core.domain.DistributionCenter;
+
+import java.util.List;
+
+public interface AllDistributionCenterUseCase {
+
+    List<DistributionCenter> getAllCenters();
+}
