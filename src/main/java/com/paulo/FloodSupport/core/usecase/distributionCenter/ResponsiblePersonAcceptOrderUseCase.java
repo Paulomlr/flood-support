bf7a4580dc18f404e0df4faf71771bf707dfd74e
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-10-25 11:47:14 -0300

    feat: add ResponsiblePersonAcceptOrderUseCase

---
+++ src/main/java/com/paulo/FloodSupport/core/usecase/distributionCenter/ResponsiblePersonAcceptOrderUseCase.java
@@ -0,0 +1,9 @@
+package com.paulo.FloodSupport.core.usecase.distributionCenter;
+
+import com.paulo.FloodSupport.core.domain.Order;
+
+public interface ResponsiblePersonAcceptOrderUseCase {
+
+    void execute(Order order);
+}
+
