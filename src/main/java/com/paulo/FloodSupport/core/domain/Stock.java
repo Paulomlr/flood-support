6de0d66a1c9beae7107fe53029e8dea973a728ab
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2024-12-15 16:31:47 -0300

    refactor: add validations in Stock

--- src/main/java/com/paulo/FloodSupport/core/domain/Stock.java
+++ src/main/java/com/paulo/FloodSupport/core/domain/Stock.java
@@ -19,11 +19,42 @@ public class Stock {
         ItemType category = item.getItemType();
         String itemName = item.getName();
 
+        if(quantity <= 0) {
+            throw new IllegalArgumentException("The quantity to be added must be greater than zero.");
+        }
+
         quantityPerCategory.put(category, quantityPerCategory.getOrDefault(category, 0) + quantity);
 
         quantityPerItem.putIfAbsent(category, new LinkedHashMap<>());
         Map<String, Integer> itemCategory = quantityPerItem.get(category);
         itemCategory.put(itemName, itemCategory.getOrDefault(itemName, 0) + quantity);
+    }
+
+    public void removeItem(Item item, int quantity) {
+        ItemType category = item.getItemType();
+        String itemName = item.getName();
+
+        int currentCategoryQuantity = quantityPerCategory.getOrDefault(category, 0);
+        if (quantity > currentCategoryQuantity) {
+            throw new IllegalArgumentException("The quantity to be removed exceeds the total available in the category");
+        }
+
+        Map<String, Integer> itemCategory = quantityPerItem.getOrDefault(category, new LinkedHashMap<>());
+        int currentItemQuantity = itemCategory.getOrDefault(itemName, 0);
+        if (quantity > currentItemQuantity) {
+            throw new IllegalArgumentException("The quantity to be removed exceeds the total items available");
+        }
+
+        quantityPerCategory.put(category, quantityPerCategory.getOrDefault(category, 0) - quantity);
+        itemCategory.put(itemName, currentItemQuantity - quantity);
+
+        if (itemCategory.get(itemName) == 0) {
+            itemCategory.remove(itemName);
+        }
+
+        if (quantityPerCategory.get(category) == 0) {
+            quantityPerCategory.remove(category);
+        }
     }
 
     public int getTotalQuantityPerCategory(ItemType itemType) {
